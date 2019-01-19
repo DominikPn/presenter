902a83e0aa950d4610696b026efd4f29755c4ca3
Soft <dom@dom-komputer>
2019-01-19 11:31:44 +0100

    Add magic methods to Presenter
    Add suport for __call and __get

--- src/Presenter.php
+++ src/Presenter.php
@@ -7,11 +7,51 @@ abstract class Presenter
     protected $methodCasts = [];
     protected $propertyCasts = [];
 
-    public function addMethodCast(string $methodName, $bind){
+    public function addMethodCast(string $methodName, $bind)
+    {
         $this->methodCasts[$methodName] = $bind;
     }
 
-    public function addPropertyCast(string $propertyName, $bind){
+    public function addPropertyCast(string $propertyName, $bind)
+    {
         $this->propertyCasts[$propertyName] = $bind;
+    }
+
+    public function __call($name, $arguments)
+    {
+        if($this->checkIfMethodExists($name))
+        {
+            if($this->isFunction($this->methodCasts[$name]))
+            {
+                array_unshift($arguments,$this);
+                return call_user_func_array($this->methodCasts[$name],$arguments);
+            }
+        }
+    }
+
+    public function __get($name)
+    {
+        if($this->checkIfPropertyExists($name))
+        {
+            if($this->isFunction($this->propertyCasts[$name]))
+            {
+                return call_user_func_array($this->propertyCasts[$name],[$this]);
+            }
+        }
+    }
+
+    private function checkIfPropertyExists(string $propertyName)
+    {
+        return array_key_exists($propertyName,$this->propertyCasts);
+    }
+
+    private function checkIfMethodExists(string $methodName)
+    {
+        return array_key_exists($methodName,$this->methodCasts);
+    }
+
+    private function isFunction($check)
+    {
+        return is_callable($check);
     }
 }
