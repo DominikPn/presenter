8430ab05f78dcb43634534a031c80e51478c84f2
Dominik Pondo <dominikpondo@gmail.com>
2019-01-22 10:41:20 +0100

    Remove forModel method form ModelBuilder

--- src/Builder/ModelBuilder.php
+++ src/Builder/ModelBuilder.php
@@ -6,9 +6,5 @@ namespace dominikpn\Presenter\Builder;
 
 class ModelBuilder extends Builder
 {
-    public function forModel($model)
-    {
-        $this->presenter->setModel($model);
-        return $this;
-    }
+
 }
