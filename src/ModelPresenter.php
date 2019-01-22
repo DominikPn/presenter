5ede04544fb9449f749cbd92cbcae099d09543a9
Dominik Pondo <dominikpondo@gmail.com>
2019-01-22 10:24:36 +0100

    Create ModelPresenter

---
+++ src/ModelPresenter.php
@@ -0,0 +1,23 @@
+<?php
+namespace dominikpn\Presenter;
+
+use dominikpn\Presenter\Exceptions\InvalidModelType;
+
+abstract class ModelPresenter
+{
+    protected $model = null;
+    protected $checkType = true;
+    
+    public function setModel($model)
+    {
+        if($this->checkType && !$this->isValidType($model)) throw new InvalidModelType();
+        $this->model = $model;
+    }
+
+    protected function isValidType($model)
+    {
+        return get_class($model) === $this->modelType();
+    }
+
+    abstract protected function modelType():string;
+}
